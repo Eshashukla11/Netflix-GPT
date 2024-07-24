a74a27a303b87c3f3325a29a089398a86e145df4
Eshashukla11 <eshashukla232001@gmail.com>
2024-07-24 21:42:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-3xl font-bold text-green-800'>
+      Come On! Let's build Netflix Gpt.
+    </div>
+  )
+}
+
+export default App
